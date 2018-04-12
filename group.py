92e2ef79e19cddbed15c76e0254d2038c4e838af
Legioner <legioner.87@list.ru>
2018-04-12 15:55:28 +0300

    2st homework

---
+++ group.py
@@ -0,0 +1,10 @@
+
+
+
+class Group:
+
+    def __init__(self,group_name, header,footer):
+        self.group_name = group_name
+        self.header = header
+        self.footer = footer
+
--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
+from group import Group
 import time, unittest
 
 
@@ -13,6 +14,7 @@ def is_alert_present(wd):
         return False
 
 class test_add_group(unittest.TestCase):
+
     def setUp(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
@@ -21,9 +23,20 @@ class test_add_group(unittest.TestCase):
         success = True
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_group(wd)
-        self.create_new_group(wd)
+        self.create_new_group(wd, Group(group_name="first group", header="Testing", footer="My first group"))
+        self.open_group(wd)
+        self.logout(wd)
+        self.assertTrue(success)
+
+    def test_test_add_empty_group(self):
+        success = True
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group(wd)
+        self.create_new_group(wd, Group(group_name=" ", header=" ", footer=" "))
         self.open_group(wd)
         self.logout(wd)
         self.assertTrue(success)
@@ -31,23 +44,24 @@ class test_add_group(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_new_group(self, wd):
+    def create_new_group(self, wd,group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("first group")
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Testing")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("My first group")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
+
 
     def open_group(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd, username="admin", password="secret"):
+    def login(self, wd, username, password):
         # Заполняем поле user
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
