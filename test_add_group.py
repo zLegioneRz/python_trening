d9480aad34762c11057875992f21311002156873
Legioner <legioner.87@list.ru>
2018-04-12 11:05:33 +0300

    it`s work!

--- test_add_group.py
+++ test_add_group.py
@@ -12,7 +12,7 @@ def is_alert_present(wd):
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
     def test_test_add_group(self):
