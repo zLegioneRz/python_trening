c328dadb7ef8f75b1db771ba6739f98b82a4b212
Legioner <legioner.87@list.ru>
2018-04-12 17:08:43 +0300

    3st homework

---
+++ contact.py
@@ -0,0 +1,29 @@
+
+
+class Contakt:
+
+    def __init__(self, wd, firstname, midlname, lastname, nickname,
+        titl, company, adress, home_num,
+        mob_nomber, work_num, fax, mail1,
+        mail2, mail3, homepage, beyer, ayer,
+        adress2, home2, notes):
+        self.firstname =firstname
+        self.midlname = midlname
+        self.lastname=lastname
+        self.nickname=nickname
+        self.titl=titl
+        self.company =company
+        self.adress = adress
+        self.home_num = home_num
+        self.mob_nomber = mob_nomber
+        self.work_num = work_num
+        self.fax = fax
+        self.mail1 = mail1
+        self.mail2 = mail2
+        self.mail3 = mail3
+        self.homepage = homepage
+        self.beyer = beyer
+        self.ayer =ayer
+        self.adress2 = adress2
+        self.home2 = home2
+        self.notes = notes
