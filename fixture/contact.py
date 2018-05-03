c8a5207736e67939edbfd26be122ebcd7ba08f2a
Legioner <legioner.87@list.ru>
2018-05-03 15:27:33 +0300

    Добавили удаление первого контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -84,3 +84,11 @@ class ContactHelper:
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(Contakt.notes)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("/html/body/div[1]/div[4]/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
+
+    #def modify_first_cont(self, ):
