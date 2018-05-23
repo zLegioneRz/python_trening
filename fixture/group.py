c4fb5c5cda9196c203c9afcd716045cf69a42354
Legioner <legioner.87@list.ru>
2018-05-23 14:25:41 +0300

    интеоектуальность нахождения на странице

--- fixture/group.py
+++ fixture/group.py
@@ -31,7 +31,8 @@ class GroupHelper:
 
     def open_group(self):
         wd = self.app.wd
-        wd.find_element_by_xpath("//div[@id='nav']/ul//a[@href='group.php']").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_xpath("//div[@id='nav']/ul//a[@href='group.php']").click()
         #wd.find_element_by_link_text("groups").click()
 
     def delete_first_group(self):
