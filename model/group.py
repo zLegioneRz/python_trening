5e0d46c5892f1b64c22f3025514d2f46046f83f4
Legioner <legioner.87@list.ru>
2018-05-30 11:12:46 +0300

    test add group сравнение

--- model/group.py
+++ model/group.py
@@ -1,5 +1,5 @@
 
-
+from sys import maxsize
 
 class Group:
 
@@ -9,4 +9,15 @@ class Group:
         self.footer = footer
         self.id = id
 
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.group_name)
 
+    def __eq__(self, other):
+        #return self.id == other.id and self.group_name == other.group_name
+        return (self.id is None or other.id is None or self.id == other.id) and (self.group_name == other.group_name)
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -2,21 +2,27 @@
 
 from model.group import Group
 
+
 #Тест с заполненными данными
 
 def test_test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(group_name="first group", header="Testing", footer="My first group"))
+    group = Group(group_name="first group", header="Testing", footer="My first group")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
     #Тест с незаполненными полями
 
 def test_test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(group_name=" ", header=" ", footer=" "))
+    group = Group(group_name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
