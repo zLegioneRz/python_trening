75e572280baa50402f43596291339f0842eb22bc
Legioner <legioner.87@list.ru>
2018-05-30 12:36:31 +0300

    подготовка к 11 ДЗ

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -6,24 +6,37 @@ def test_test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(group_name="test"))
     old_groups = app.group.get_group_list()
-    app.group.modyfy_first_group_new(Group(group_name='New group'))
+    group = Group(group_name='New group')
+    group.id = old_groups[0].id
+    app.group.modyfy_first_group_new(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(group_name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.modyfy_first_group_new(Group(header='New header'))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
 
+# def test_test_modify_group_header(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(group_name="test"))
+#     old_groups = app.group.get_group_list()
+#     group = Group(header='New header')
+#     group.id = old_groups[0].id
+#     app.group.modyfy_first_group_new(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#     old_groups[0] = group
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_test_modify_group_footer(app):
-    if app.group.count() == 0:
-        app.group.create(Group(group_name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.modyfy_first_group_new(Group(footer='New footer'))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+#
+# def test_test_modify_group_footer(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(group_name="test"))
+#     old_groups = app.group.get_group_list()
+#     group = Group(footer='New footer')
+#     group.id = old_groups[0].id
+#     app.group.modyfy_first_group_new(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#     old_groups[0] = group
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
